7c754bb5470ba76ec3761fdae3b1ec52b9a8dd69
Squill3x <squill3x@hotmail.com>
2017-03-09 16:38:52 +0000

    Complete Booking constructor test.
    Change Booking constructor to prevent inconsistent dates and throwing HotelException.
    Change Booking conflit method to throw HotelException on conflict.

--- hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/domain/Booking.java
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/domain/Booking.java
@@ -2,6 +2,8 @@ package pt.ulisboa.tecnico.softeng.hotel.domain;
 
 import org.joda.time.LocalDate;
 
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+
 public class Booking {
 	private static int counter = 0;
 
@@ -9,7 +11,11 @@ public class Booking {
 	private final LocalDate arrival;
 	private final LocalDate departure;
 
-	Booking(Hotel hotel, LocalDate arrival, LocalDate departure) {
+	Booking(Hotel hotel, LocalDate arrival, LocalDate departure) throws HotelException {
+		if(hotel == null || arrival == null || departure == null || checkEqualOrAfter(arrival, departure)) {
+			throw new HotelException();
+		}
+		
 		this.reference = hotel.getCode() + Integer.toString(++Booking.counter);
 		this.arrival = arrival;
 		this.departure = departure;
@@ -27,20 +33,25 @@ public class Booking {
 		return this.departure;
 	}
 
-	boolean conflict(LocalDate arrival, LocalDate departure) {
-		if (arrival.isAfter(this.arrival) && arrival.isBefore(this.departure)) {
-			return true;
-		}
-
-		if (departure.isAfter(this.arrival) && departure.isBefore(this.departure)) {
-			return true;
-		}
-
-		if (arrival.isBefore(this.arrival) && departure.isAfter(this.departure)) {
-			return true;
+	boolean conflict(LocalDate arrival, LocalDate departure) throws HotelException {
+		if (arrival.isAfter(this.arrival) && arrival.isBefore(this.departure)
+			|| departure.isAfter(this.arrival) && departure.isBefore(this.departure)
+			|| arrival.isBefore(this.arrival) && departure.isAfter(this.departure)) {
+			throw new HotelException();
 		}
 
 		return false;
 	}
-
+	
+	private boolean checkEqualOrAfter(LocalDate date, LocalDate refDate) {
+		return date.isEqual(refDate) || date.isAfter(refDate);
+	}
+	
+	public static int getCounter() {
+		return Booking.counter;
+	}
+	
+	public static void resetCounter() {
+		Booking.counter = 0;
+	}
 }
--- hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/BookingConstructorTest.java
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/BookingConstructorTest.java
@@ -3,28 +3,104 @@ package pt.ulisboa.tecnico.softeng.hotel.domain;
 import org.joda.time.LocalDate;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+
 public class BookingConstructorTest {
-
+	private Hotel hotel;
+	int counter = 0;
+	private int multiple = 10;
+	
+	@Before
+	public void setUp() {
+		hotel = new Hotel("HOTELID", "HotelName");
+	}
+	
 	@Test
-	public void success() {
-		Hotel hotel = new Hotel("XPTO123", "Londres");
-
-		LocalDate arrival = new LocalDate(2016, 12, 19);
-		LocalDate departure = new LocalDate(2016, 12, 21);
-
-		Booking booking = new Booking(hotel, arrival, departure);
-
-		Assert.assertTrue(booking.getReference().startsWith(hotel.getCode()));
-		Assert.assertTrue(booking.getReference().length() > Hotel.CODE_SIZE);
-		Assert.assertEquals(arrival, booking.getArrival());
-		Assert.assertEquals(departure, booking.getDeparture());
+	public void successSingle() {
+		LocalDate arrival = new LocalDate(2016, 12, 10);
+		LocalDate departure = new LocalDate(2016, 12, 15);
+		
+		createBooking(hotel, arrival, departure);
+	}
+	
+	@Test(expected = HotelException.class)
+	public void failureSingleNull() {
+		createBooking(null, null, null);
+	}
+	
+	@Test(expected = HotelException.class)
+	public void failureSingleDate() {
+		LocalDate arrival = new LocalDate(2016, 12, 15);
+		LocalDate departure = new LocalDate(2016, 12, 10);
+		
+		createBooking(hotel, arrival, departure);
+	}
+	
+	@Test
+	public void successMultiple() {
+		LocalDate arrival = new LocalDate(2016, 12, 10);
+		LocalDate departure = new LocalDate(2016, 12, 15);
+		
+		for(int index = 0; index < multiple; index++) {
+			createBooking(hotel, arrival, departure);
+		}
+	}
+	
+	@Test(expected = HotelException.class)
+	public void failureMultipleNull() {
+		LocalDate arrival = new LocalDate(2016, 12, 10);
+		LocalDate departure = new LocalDate(2016, 12, 15);
+		
+		for(int index = 0; index < multiple - 1; index++) {
+			createBooking(hotel, arrival, departure);
+		}
+		
+		createBooking(null, null, null);
+	}
+	
+	@Test(expected = HotelException.class)
+	public void failureMultipleDate() {
+		LocalDate goodArrival = new LocalDate(2016, 12, 10);
+		LocalDate goodDeparture = new LocalDate(2016, 12, 15);
+		LocalDate badArrival = new LocalDate(2016, 12, 15);
+		LocalDate badDeparture = new LocalDate(2016, 12, 10);
+		
+		for(int index = 0; index < multiple - 1; index++) {
+			createBooking(hotel, goodArrival, goodDeparture);
+		}
+		
+		createBooking(hotel, badArrival, badDeparture);
 	}
 
 	@After
 	public void tearDown() {
+		counter = 0;
 		Hotel.hotels.clear();
+		Booking.resetCounter();
 	}
-
+	
+	private void createBooking(Hotel hotel, LocalDate arrival, LocalDate departure) throws HotelException {
+		Booking booking = null;
+		
+		try {
+			booking = new Booking(hotel, arrival, departure);
+			counter = Integer.parseInt(booking.getReference().substring(Hotel.CODE_SIZE));
+			
+			Assert.assertTrue(booking.getReference().startsWith(hotel.getCode()));
+			Assert.assertTrue(booking.getReference().length() > Hotel.CODE_SIZE);
+			
+			Assert.assertEquals(arrival, booking.getArrival());
+			Assert.assertEquals(departure, booking.getDeparture());
+		}
+		catch(HotelException e) {
+			Assert.assertNull(booking);
+			throw e;
+		}
+		finally {
+			Assert.assertEquals(Booking.getCounter(), counter);
+		}
+	}
 }
