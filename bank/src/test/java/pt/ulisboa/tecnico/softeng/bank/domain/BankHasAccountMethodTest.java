25bf593358a68179a9ab1cc89d2407aa6ae04ab0
Viktor Vasylkovskyi <viktor.vasylkovskyi@tecnico.ulisboa.pt>
2017-03-14 12:31:06 +0000

    Test bank getAccount method.

--- bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankHasAccountMethodTest.java
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankHasAccountMethodTest.java
@@ -5,9 +5,13 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+
+
 public class BankHasAccountMethodTest {
 	Bank bank;
 	Client client;
+
 
 	@Before
 	public void setUp() {
@@ -24,6 +28,38 @@ public class BankHasAccountMethodTest {
 		Assert.assertEquals(account, result);
 	}
 
+	/* -------------------------------- */
+	
+	@Test(expected = BankException.class)
+	public void emptyAccountSet() {
+		Bank bank = new Bank("Dollar", "AE23");
+		bank.getAccount("InvalidIBAN");
+	}
+	
+	
+	@Test(expected = BankException.class)
+	public void nullIBAN() {
+		this.bank.getAccount(null);
+	}
+	@Test(expected = BankException.class)
+	public void emptyIBAN() {
+		this.bank.getAccount("");
+	}
+	
+	@Test(expected = BankException.class)
+	public void IBANwithBlanks() {
+		this.bank.getAccount("    ");
+	}
+
+	
+	@Test(expected = BankException.class)
+	public void noMatchForIBAN() {
+			bank.getAccount("-123456789");
+	}
+
+	/* ------------------------------- */
+	
+	
 	@After
 	public void tearDown() {
 		Bank.banks.clear();
