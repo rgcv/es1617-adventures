96938abdf764831bc7a0c632b06a8b7de0e9910a
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-03-14 10:48:28 +0000

    Test and fix Adventure Constructor

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
@@ -3,7 +3,7 @@ package pt.ulisboa.tecnico.softeng.broker.domain;
 import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import pt.ulisboa.tecnico.softeng.broker.exception.BrokerException;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
@@ -26,12 +26,21 @@ public class Adventure {
 	private String activityBooking;
 
 	public Adventure(Broker broker, LocalDate begin, LocalDate end, int age, String IBAN, int amount) {
+		checkBroker(broker);
 		this.ID = broker.getCode() + Integer.toString(++counter);
 		this.broker = broker;
+
+		checkDates(begin, end);
 		this.begin = begin;
 		this.end = end;
+
+		checkAge(age);
 		this.age = age;
+
+		checkIBAN(IBAN);
 		this.IBAN = IBAN;
+
+		checkAmount(amount);
 		this.amount = amount;
 
 		broker.addAdventure(this);
@@ -41,8 +50,21 @@ public class Adventure {
 		return this.ID;
 	}
 
+	private void checkBroker(Broker broker) {
+		if (broker == null) {
+			throw new BrokerException();
+		}
+	}
+
 	public Broker getBroker() {
 		return this.broker;
+	}
+
+	private void checkDates(LocalDate begin, LocalDate end) {
+		if (begin == null || end == null ||
+				end.minusDays(1).isBefore(begin)) {
+			throw new BrokerException();
+		}
 	}
 
 	public LocalDate getBegin() {
@@ -53,12 +75,30 @@ public class Adventure {
 		return this.end;
 	}
 
+	private void checkAge(int age) {
+		if (!(age >= 18 && age <= 100)) {
+			throw new BrokerException();
+		}
+	}
+
 	public int getAge() {
 		return this.age;
 	}
 
+	private void checkIBAN(String IBAN) {
+		if (IBAN == null || IBAN.trim().length() == 0) {
+			throw new BrokerException();
+		}
+	}
+
 	public String getIBAN() {
 		return this.IBAN;
+	}
+
+	private void checkAmount(int amount) {
+		if (amount <= 0) {
+			throw new BrokerException();
+		}
 	}
 
 	public int getAmount() {
--- broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/AdventureConstructorMethodTest.java
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/AdventureConstructorMethodTest.java
@@ -5,9 +5,16 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import pt.ulisboa.tecnico.softeng.broker.exception.BrokerException;
 
 public class AdventureConstructorMethodTest {
+	public static final String IBAN = "BK011234567";
+	public static final int AGE = 20;
+	public static final int AMOUNT = 300;
+
 	private Broker broker;
+	private LocalDate begin = new LocalDate(2016, 12, 19);
+	private LocalDate end   = new LocalDate(2016, 12, 21);
 
 	@Before
 	public void setUp() {
@@ -16,22 +23,139 @@ public class AdventureConstructorMethodTest {
 
 	@Test
 	public void success() {
-		LocalDate begin = new LocalDate(2016, 12, 19);
-		LocalDate end = new LocalDate(2016, 12, 21);
-
-		Adventure adventure = new Adventure(this.broker, begin, end, 20, "BK011234567", 300);
+		Adventure adventure = new Adventure(this.broker, this.begin, this.end, AGE, IBAN, AMOUNT);
 
 		Assert.assertEquals(this.broker, adventure.getBroker());
-		Assert.assertEquals(begin, adventure.getBegin());
-		Assert.assertEquals(end, adventure.getEnd());
-		Assert.assertEquals(20, adventure.getAge());
-		Assert.assertEquals("BK011234567", adventure.getIBAN());
-		Assert.assertEquals(300, adventure.getAmount());
+		Assert.assertEquals(this.begin, adventure.getBegin());
+		Assert.assertEquals(this.end, adventure.getEnd());
+		Assert.assertEquals(AGE, adventure.getAge());
+		Assert.assertEquals(IBAN, adventure.getIBAN());
+		Assert.assertEquals(AMOUNT, adventure.getAmount());
 		Assert.assertTrue(this.broker.hasAdventure(adventure));
 
 		Assert.assertNull(adventure.getBankPayment());
 		Assert.assertNull(adventure.getActivityBooking());
 		Assert.assertNull(adventure.getRoomBooking());
+	}
+
+	@Test
+	public void nullBroker() {
+		try {
+			new Adventure(null, this.begin, this.end, AGE, IBAN, AMOUNT);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+	public void nullBeginDate() {
+		try {
+			new Adventure(this.broker, null, this.end, AGE, IBAN, AMOUNT);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+	public void nullEndDate() {
+		try {
+			new Adventure(this.broker, this.begin, null, AGE, IBAN, AMOUNT);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+	public void endDateBeforeBegin() {
+		try {
+			new Adventure(this.broker, this.end, this.begin, AGE, IBAN, AMOUNT);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+    public void atLeastOneDayApart() {
+	    try {
+	        new Adventure(this.broker, this.begin, this.begin, AGE, IBAN, AMOUNT);
+	        Assert.fail();
+        } catch(BrokerException be) {
+	        Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+        }
+    }
+
+	@Test
+	public void ageLessThanMinimum() {
+		try {
+			new Adventure(this.broker, this.begin, this.end, 17, IBAN, AMOUNT);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+	public void ageGreaterThanMaximum() {
+		try {
+			new Adventure(this.broker, this.begin, this.end, 101, IBAN, AMOUNT);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+	public void nullIBAN() {
+		try {
+			new Adventure(this.broker, this.begin, this.end, AGE, null, AMOUNT);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+	public void emptyIBAN() {
+		try {
+			new Adventure(this.broker, this.begin, this.end, AGE, "", AMOUNT);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+	public void blankIBAN() {
+		try {
+			new Adventure(this.broker, this.begin, this.end, AGE, "    ", AMOUNT);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+	public void negativeAmount() {
+		try {
+			new Adventure(this.broker, this.begin, this.end, AGE, IBAN, -1);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
+	}
+
+	@Test
+	public void zeroAmount() {
+		try {
+			new Adventure(this.broker, this.begin, this.end, AGE, IBAN, 0);
+			Assert.fail();
+		} catch(BrokerException be) {
+			Assert.assertEquals(0, this.broker.getNumberOfAdventures());
+		}
 	}
 
 	@After
