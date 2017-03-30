a2927c24eb1e04c3886c640a0906f1d181686c29
Goncalo Baptista <goncalo.baptista@tecnico.ulisboa.pt>
2017-03-30 17:19:15 +0100

    Implement a test class for ReserveActivityState
    Close #30

--- broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/ReserveActivityStateProcessMethodTest.java
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/ReserveActivityStateProcessMethodTest.java
@@ -1,5 +1,101 @@
 package pt.ulisboa.tecnico.softeng.broker.domain;
 
-public class ReserveActivityStateProcessMethodTest {
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import mockit.Injectable;
+import mockit.Mocked;
+import mockit.StrictExpectations;
+import mockit.integration.junit4.JMockit;
+import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
+
+@RunWith(JMockit.class)
+public class ReserveActivityStateProcessMethodTest {
+    private static final int AGE = 20;
+    private static final String IBAN = "BK01987654321";
+    private static final int AMOUNT = 300;
+	private static final String ACTIVITY_CONFIRMATION = "ActivityConfirmation";
+    private final LocalDate begin = new LocalDate(2016, 12, 19);
+    private final LocalDate end = new LocalDate(2016, 12, 21);
+
+    private Adventure adventure;
+
+    @Injectable
+    private Broker broker;
+
+    @Before
+    public void setUp() {
+        this.adventure = new Adventure(this.broker, this.begin, this.end, AGE, IBAN, AMOUNT);
+        this.adventure.setState(Adventure.State.RESERVE_ACTIVITY);
+    }
+    
+    @Test
+    public void reserveActivityDifferentDates(@Mocked final ActivityInterface activityInterface) {
+    	new StrictExpectations() {
+    		{
+    			ActivityInterface.reserveActivity(begin, end, AGE);
+				this.result = ACTIVITY_CONFIRMATION;
+    		}
+    	};
+    	
+    	this.adventure.process();
+    	
+    	Assert.assertEquals(ACTIVITY_CONFIRMATION, this.adventure.getActivityConfirmation());
+        Assert.assertEquals(State.BOOK_ROOM, this.adventure.getState());
+    }
+    
+    @Test
+    public void reserveActivityEqualDates(@Mocked final ActivityInterface activityInterface) {
+    	Adventure adventure2 = new Adventure(this.broker, this.begin, this.begin, AGE, IBAN, AMOUNT);
+    	new StrictExpectations() {
+    		{
+    			ActivityInterface.reserveActivity(begin, begin, AGE);
+				this.result = ACTIVITY_CONFIRMATION;
+    		}
+    	};
+    	
+    	adventure2.process();
+    	
+    	Assert.assertEquals(ACTIVITY_CONFIRMATION, adventure2.getActivityConfirmation());
+        Assert.assertEquals(State.CONFIRMED, adventure2.getState());
+    }
+    
+    @Test
+    public void reserveActivityActivityException(@Mocked final ActivityInterface activityInterface) {
+    	new StrictExpectations() {
+    		{
+    			ActivityInterface.reserveActivity(begin, end, AGE);
+    			this.result = new ActivityException();
+    		}
+    	};
+    	
+    	this.adventure.process();
+    	
+    	Assert.assertEquals(State.UNDO, this.adventure.getState());
+    }
+    
+    @Test
+    public void reserveActivityRemoteAccessException(@Mocked final ActivityInterface activityInterface) {
+    	new StrictExpectations() {
+    		{
+    			ActivityInterface.reserveActivity(begin, end, AGE);
+    			this.times = 5; //ReserveActivityState.MAX_REMOTE_ERRORS;
+    			this.result = new RemoteAccessException();
+    		}
+    	};
+    	
+    	for (int i = 0; i < 5/*ReserveActivityState.MAX_REMOTE_ERRORS*/; i++) {
+    		this.adventure.process();
+    	}
+    	
+    	Assert.assertEquals(State.UNDO, this.adventure.getState());
+    }
+    
+    
 }
