675ab1862c82430f6fe400e83ddd482f93e22c94
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-04-08 21:41:33 +0100

    Add fields to Activity in dml, making them persistent
    Closes #66

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Activity.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Activity.java
@@ -13,21 +13,16 @@ public class Activity extends Activity_Base {
 
     private static int counter = 0;
 
-    private final String name;
-    private final String code;
-    private final int minAge;
-    private final int maxAge;
-    private final int capacity;
     private final Set<ActivityOffer> offers = new HashSet<>();
 
     public Activity(ActivityProvider provider, String name, int minAge, int maxAge, int capacity) {
         checkArguments(provider, name, minAge, maxAge, capacity);
 
-        this.code = provider.getCode() + Integer.toString(++Activity.counter);
-        this.name = name;
-        this.minAge = minAge;
-        this.maxAge = maxAge;
-        this.capacity = capacity;
+        setCode(provider.getCode() + Integer.toString(++Activity.counter));
+        setName(name);
+        setMinAge(minAge);
+        setMaxAge(maxAge);
+        setCapacity(capacity);
 
         provider.addActivity(this);
     }
@@ -45,26 +40,6 @@ public class Activity extends Activity_Base {
             throw new ActivityException();
         }
 
-    }
-
-    String getName() {
-        return this.name;
-    }
-
-    String getCode() {
-        return this.code;
-    }
-
-    int getMinAge() {
-        return this.minAge;
-    }
-
-    int getMaxAge() {
-        return this.maxAge;
-    }
-
-    int getCapacity() {
-        return this.capacity;
     }
 
     int getNumberOfOffers() {
@@ -86,7 +61,7 @@ public class Activity extends Activity_Base {
     }
 
     boolean matchAge(int age) {
-        return age >= this.minAge && age <= this.maxAge;
+        return age >= getMinAge() && age <= getMaxAge();
     }
 
     public Booking getBooking(String reference) {
