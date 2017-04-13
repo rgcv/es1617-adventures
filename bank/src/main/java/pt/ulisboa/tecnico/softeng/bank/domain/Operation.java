51dc896648cc73e417039178e36d648fe416bda5
Squill3x <squill3x@hotmail.com>
2017-04-13 15:03:30 +0100

    Make Operation's fields persistent
    Close #65

--- bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Operation.java
+++ bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Operation.java
@@ -4,68 +4,49 @@ import org.joda.time.DateTime;
 
 import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
 
-public class Operation extends Operation_Base{
+public class Operation extends Operation_Base {
     public static enum Type {
         DEPOSIT, WITHDRAW
     };
 
     private static int counter = 0;
 
-    private final String reference;
-    private final Type type;
-    private final Account account;
-    private final int value;
-    private final DateTime time;
+    Account account;
 
     public Operation(Type type, Account account, int value) {
         checkArguments(type, account, value);
 
-        this.reference = account.getBank().getCode() + Integer.toString(++Operation.counter);
-        this.type = type;
+        setReference(account.getBank().getCode() + Integer.toString(++Operation.counter));
+        setType(type);
         this.account = account;
-        this.value = value;
-        this.time = DateTime.now();
+        setValue(value);
+        setTime(DateTime.now());
 
         account.getBank().addLog(this);
     }
 
     private void checkArguments(Type type, Account account, int value) {
-        if (type == null || account == null || value <= 0) {
+        if(type == null || account == null || value <= 0) {
             throw new BankException();
         }
-    }
-
-    public String getReference() {
-        return this.reference;
-    }
-
-    public Type getType() {
-        return this.type;
     }
 
     public Account getAccount() {
         return this.account;
     }
 
-    public int getValue() {
-        return this.value;
-    }
-
-    public DateTime getTime() {
-        return this.time;
-    }
-
     public String revert() {
-        switch (this.type) {
-        case DEPOSIT:
-            return this.account.withdraw(this.value);
-        case WITHDRAW:
-            return this.account.deposit(this.value);
-        default:
-            throw new BankException();
-
+        switch(getType()) {
+            case DEPOSIT:
+                return getAccount().withdraw(getValue());
+            case WITHDRAW:
+                return getAccount().deposit(getValue());
+            default:
+                throw new BankException();
         }
-
     }
 
+    public void delete() {
+    	deleteDomainObject();
+    }
 }
