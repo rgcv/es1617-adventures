333713c059f108c85e62def58a29fb66b2bd3e83
Joana Arezes <joanarezes@outlook.com>
2017-04-28 23:37:50 +0100

    Add class RoomData

---
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/services/local/dataobjects/RoomData.java
@@ -0,0 +1,57 @@
+package pt.ulisboa.tecnico.softeng.hotel.services.local.dataobjects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pt.ulisboa.tecnico.softeng.hotel.domain.Booking;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Hotel;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room;
+
+public class RoomData {
+	private Hotel hotel;
+	private String number;
+	private Room.Type type;
+	private List<Booking> bookings = new ArrayList<>();
+	
+	public RoomData() {
+	}
+	
+	public RoomData(Room room) {
+		this.hotel = room.getHotel();
+		this.number = room.getNumber();
+		this.type = room.getType();
+		
+	}
+	
+	public Hotel getHotel() {
+		return hotel;
+	}
+	
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public Room.Type getType() {
+		return type;
+	}
+
+	public void setType(Room.Type type) {
+		this.type = type;
+	}
+	
+	public List<Booking> getBookings() {
+		return bookings;
+	}
+	
+	public void setBookings(List<Booking> bookings) {
+		this.bookings = bookings;
+	}
+}
