a5f0263b97e09cd9bcfe162477dc7fe2843dbd8b
Goncalo Baptista <goncalo.baptista@tecnico.ulisboa.pt>
2017-04-29 18:47:35 +0100

    Add createBank and getBanks methods
    In #100

--- bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/services/local/BankInterface.java
+++ bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/services/local/BankInterface.java
@@ -1,51 +1,70 @@
 package pt.ulisboa.tecnico.softeng.bank.services.local;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.Atomic.TxMode;
 import pt.ist.fenixframework.FenixFramework;
 import pt.ulisboa.tecnico.softeng.bank.domain.Bank;
 import pt.ulisboa.tecnico.softeng.bank.domain.Operation;
 import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+import pt.ulisboa.tecnico.softeng.bank.services.local.dataobjects.BankData;
+import pt.ulisboa.tecnico.softeng.bank.services.local.dataobjects.BankData.CopyDepth;
 import pt.ulisboa.tecnico.softeng.bank.services.local.dataobjects.BankOperationData;
 
 public class BankInterface {
 
-    @Atomic(mode = TxMode.WRITE)
-    public static String processPayment(String IBAN, int amount) {
-        for (Bank bank : FenixFramework.getDomainRoot().getBankSet()) {
-            if (bank.getAccount(IBAN) != null) {
-                return bank.getAccount(IBAN).withdraw(amount);
-            }
-        }
-        throw new BankException();
-    }
+	@Atomic(mode = TxMode.WRITE)
+	public static void createBank(BankData bankData) {
+		new Bank(bankData.getName(), bankData.getCode());
+	}
 
-    @Atomic(mode = TxMode.WRITE)
-    public static String cancelPayment(String paymentConfirmation) {
-        Operation operation = getOperationByReference(paymentConfirmation);
-        if (operation != null) {
-            return operation.revert();
-        }
-        throw new BankException();
-    }
+	@Atomic(mode = TxMode.READ)
+	public static List<BankData> getBanks() {
+		List<BankData> banks = new ArrayList<>();
+		for (Bank bank : FenixFramework.getDomainRoot().getBankSet()) {
+			banks.add(new BankData(bank, CopyDepth.SHALLOW));
+		}
+		return banks;
+	}
 
-    @Atomic(mode = TxMode.READ)
-    public static BankOperationData getOperationData(String reference) {
-        Operation operation = getOperationByReference(reference);
-        if (operation != null) {
-            return new BankOperationData(operation);
-        }
-        throw new BankException();
-    }
+	@Atomic(mode = TxMode.WRITE)
+	public static String processPayment(String IBAN, int amount) {
+		for (Bank bank : FenixFramework.getDomainRoot().getBankSet()) {
+			if (bank.getAccount(IBAN) != null) {
+				return bank.getAccount(IBAN).withdraw(amount);
+			}
+		}
+		throw new BankException();
+	}
 
-    private static Operation getOperationByReference(String reference) {
-        for (Bank bank : FenixFramework.getDomainRoot().getBankSet()) {
-            Operation operation = bank.getOperation(reference);
-            if (operation != null) {
-                return operation;
-            }
-        }
-        return null;
-    }
+	@Atomic(mode = TxMode.WRITE)
+	public static String cancelPayment(String paymentConfirmation) {
+		Operation operation = getOperationByReference(paymentConfirmation);
+		if (operation != null) {
+			return operation.revert();
+		}
+		throw new BankException();
+	}
+
+	@Atomic(mode = TxMode.READ)
+	public static BankOperationData getOperationData(String reference) {
+		Operation operation = getOperationByReference(reference);
+		if (operation != null) {
+			return new BankOperationData(operation);
+		}
+		throw new BankException();
+	}
+
+	private static Operation getOperationByReference(String reference) {
+		for (Bank bank : FenixFramework.getDomainRoot().getBankSet()) {
+			Operation operation = bank.getOperation(reference);
+			if (operation != null) {
+				return operation;
+			}
+		}
+		return null;
+	}
 
 }
